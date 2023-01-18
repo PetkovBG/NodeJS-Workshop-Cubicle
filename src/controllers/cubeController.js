a9e6b18b1dbe4ca962f0cddf30fa999ca8d3e816
PetkovBG <97334281+PetkovBG@users.noreply.github.com>
2023-01-18 22:10:33 +0200

    Modify static model save

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -10,7 +10,7 @@ exports.postCreateCube = (req, res) => {
     const { name, description, imageUrl, difficultyLevel } = req.body;
     let cube = new Cube(name, description, imageUrl, difficultyLevel); 
 
-    Cube.save(cube);
+    cube.save();
 
     res.redirect('/');
 }
