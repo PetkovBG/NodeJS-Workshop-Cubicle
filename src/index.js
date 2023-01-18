33447eda7a7da0bc33c72363dff8801f34d5fc39
PetkovBG <97334281+PetkovBG@users.noreply.github.com>
2023-01-18 18:40:50 +0200

    Setup express and env variables

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('First');
+const express = require('express');
+const config = require('./config');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Home page')
+});
+
+app.listen(config.PORT, () => console.log(`Server is now running on port ${config.PORT}...`));
