0289b1c96703110480d568a5a8cbc2e5d5bfcc9f
PetkovBG <97334281+PetkovBG@users.noreply.github.com>
2023-01-18 20:11:32 +0200

    Add home controller

--- src/routes.js
+++ src/routes.js
@@ -1,24 +1,18 @@
+const router = require('express').Router();
 const cubeController = require('./controllers/cubeController');
 
-const router = require('express').Router();
+
+const homeController = require('./controllers/homeController');
 
 //The above router setup can be done with the method below
 // const express = require('express');
 // const Router = express.Router;
 // const router = Router();
 
-router.get('/', (req, res) => {
-    res.render('index');
-});
+router.get('/', homeController.getHomePage);
 
-router.get('/about', (req, res) => {
-    res.render('about');
-});
+router.get('/about', homeController.getAboutPage);
 
-// router.get('/create', (req, res) => {
-//     res.render('create');
-// });
-
-router.get('./create', cubeController.getCreateCube);
+router.get('/create', cubeController.getCreateCube);
 
 module.exports = router;
