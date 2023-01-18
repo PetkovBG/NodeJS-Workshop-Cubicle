c79dd8f660e5af06baa8af9dc1123293fb2d589f
PetkovBG <97334281+PetkovBG@users.noreply.github.com>
2023-01-18 21:17:15 +0200

    Adding JSON database and setting up the browse menu

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -6,7 +6,9 @@ exports. getCreateCube = (req, res) => {
 };
 
 exports.postCreateCube = (req, res) => {
-    let cube = new Cube(req.body); 
+
+    const { name, description, imageUrl, difficultyLevel } = req.body;
+    let cube = new Cube(name, description, imageUrl, difficultyLevel); 
 
     Cube.save(cube);
 
