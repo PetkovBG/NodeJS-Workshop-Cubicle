f255fb7080eb4f86af05b4790e3f04405f091d8e
PetkovBG <97334281+PetkovBG@users.noreply.github.com>
2023-01-26 21:13:52 +0200

    Attach accessory to cube

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -35,3 +35,15 @@ const accessories = await Accessory.find().lean();
 
     res.render('cube/attach', { cube, accessories });
 }
+
+exports.postAttachAccessory = async (req, res) => {
+    const cube = await Cube.findById(req.params.cubeId);
+
+    const accessoryId = req.body.accessory;
+
+    cube.accessories.push(accessoryId);
+
+        cube.save();
+
+        res.redirect(`/cubes/${cube._id}/details`)
+}
