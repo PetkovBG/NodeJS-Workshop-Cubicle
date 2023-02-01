6317d71d9e20ab99b3cff609b4bf2ea769f882ba
PetkovBG <97334281+PetkovBG@users.noreply.github.com>
2023-02-01 19:33:54 +0200

    Register user functionality

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,6 +2,8 @@
 
 const router = require('express').Router();
 
+const authService = require('../services/authService');
+
 router.get('/login', (req, res) => {
     res.render('auth/login')
 })
@@ -10,14 +12,28 @@ router.get('/register', (req, res) => {
     res.render('auth/register')
 });
 
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
     const {username, password, repeatPassword } = req.body;
 
     if(password !== repeatPassword) {
-        return res.status(404).end();
+        return res.redirect('/404'); 
     }
 
+    const existingUser = await authService.getUserByUsername(username);
+
+    if(existingUser) {
+
+        return res.redirect('/404'); 
+        
     
+    }
+
+    const user = await authService.register(username, password);
+
+    console.log(user);
+
+    res.redirect('/login');
+
 })
 
 module.exports = router;
